3d4267396ba7a35a47b935e37d7137d5d88e3048
Bruce Herr II <bherr@indiana.edu>
2018-05-04 15:29:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Example Firebase Database
+
+This example shows how to add objects to a Firebase Database.
+
+## Setup
+
+-   Sign up for a Firebase account.
+-   : `npm install`
+-   Login to firebase: `npm run login`
+-   Initialize firebase database: `npm run setup`
+    -   When presented with options, select Database and Hosting.
+    -   It will generate a number of files to use.
+-   Add the database config: `npm run create-config`
+-   Push test to hosting: `npm run deploy`
+
+```
+# Setup environment
+npm install
+
+# Login to Firebase
+npm run login
+
+# Initialize Firebase Database and Hosting
+npm run setup
+
+# Create the database config
+npm run create-config
+
+# Deploy to Firebase Hosting
+npm run deploy
+
+# A URL will be displayed that will take you to the click test site
+```
---
+++ public/main.js
@@ -0,0 +1,62 @@
+// Assumes config.js was included before this script
+firebase.initializeApp(config);
+var database = firebase.database();
+var clicksRef = firebase.database().ref('clicks');
+
+var responseId = getResponseId();
+var leftClicked = false;
+var rightClicked = false;
+
+function getResponseId() {
+  return getParameterByName('responseId') || 'NULL';
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function writeClick(responseId, circle, x, y, date) {
+  var newClicksRef = clicksRef.push();
+  newClicksRef.set({
+    responseId: responseId,
+    circle: circle,
+    x: x,
+    y: y,
+    date: date.toUTCString(),
+    timestamp: date.getTime()
+  });
+}
+
+function getRelativeXY(event) {
+  var element = document.getElementById("box");
+  var x = event.pageX - element.offsetLeft + 320;
+  var y = event.pageY - element.offsetTop + 240;
+  return [x, y];
+}
+
+function topLeftClicked(event) {
+  var xy = getRelativeXY(event);
+  writeClick(responseId, 'top-left', xy[0], xy[1], new Date());
+  leftClicked = true;
+  possiblyShowPopup()
+}
+
+function bottomRightClicked(event) {
+  var xy = getRelativeXY(event);
+  writeClick(responseId, 'bottom-right', xy[0], xy[1], new Date());
+  rightClicked = true;
+  possiblyShowPopup();
+}
+
+function possiblyShowPopup() {
+  if (leftClicked && rightClicked) {
+    var element = document.getElementById("popup");
+    element.classList.remove("hidden");
+  }
+}
